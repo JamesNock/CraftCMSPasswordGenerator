1e37ab144f02af72640b8aa2e5d993377b1b2ca3
James Nock <james@james-nock.co.uk>
2021-07-11 16:10:08 +0100

    Add complete v1 website

---
+++ components/PasswordGenerator.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <div class="p-4 border border-solid border-2 border-black">
+      <p class="text-gray-600">
+        Enter the password that you want to use e.g. 'password'.
+      </p>
+      <form class="my-4 flex">
+        <input
+          v-model="password"
+          required
+          class="flex-grow border border-solid border-gray-600 p-2"
+          type="text"
+          name="password"
+        />
+        <input
+          class="
+            flex-1
+            bg-pink-600
+            border border-solid border-gray-600 border-l-0
+            text-white
+            p-2
+            w-16
+            hover:bg-pink-400 hover:text-purple-700
+          "
+          type="submit"
+          value="Gimme!"
+          @click.prevent="fetch"
+        />
+      </form>
+      <div v-if="hashedPW" class="bg-yellow-300 p-4" @click="copyThis">
+        {{ hashedPW }}
+      </div>
+      <div v-if="errorMsg" class="bg-red-500 p-4">{{ errorMsg }}</div>
+    </div>
+    <div v-if="hashedPW && !fetching" class="text-gray-600">
+      <h4 class="py-4 text-xl font-semibold tracking-wide">How to use</h4>
+      <p>
+        Open your `users` table and find the id of the user that you want to
+        assign the password to.
+      </p>
+      <p class="mt-4">
+        Either paste the new password into the password column using your GUI or
+        run the below:
+      </p>
+      <p class="mt-4 p-2 font-mono bg-yellow-300" @click="copyThis">
+        UPDATE `users`<br />SET password="{{ hashedPW }}"<br />WHERE id="<span
+          class="
+            px-1
+            w-8
+            min-w-min
+            text-center
+            inline-block
+            align-middle
+            border-solid border-b-2 border-gray-400
+          "
+          contenteditable="true"
+          @click.prevent="prevent"
+        ></span
+        >";
+      </p>
+      <p class="mt-4">
+        <span class="font-bold">Tip</span>: type in the user ID above and then
+        click on the SQL statement to copy it.
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      password: '',
+      hashedPW: '',
+      errorMsg: '',
+      fetching: false,
+    }
+  },
+  methods: {
+    prevent(event) {
+      event.stopPropagation()
+    },
+    async fetch() {
+      if (!this.password) {
+        this.errorMsg = 'Please enter a password. e.g. password'
+        setTimeout(() => {
+          this.errorMsg = ''
+        }, 5000)
+        return
+      }
+      this.hashedPW = 'Fetching...'
+      this.fetching = true
+      this.errorMsg = ''
+      const formData = new FormData()
+      formData.append('pw', this.password)
+      await this.$axios({
+        method: 'post',
+        url: 'tools/craftcmspwgen.php',
+        data: formData,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
+        .then((res) => {
+          if (!res.data.error) {
+            this.hashedPW = res.data.message
+          } else {
+            this.hashedPW = ''
+            this.errorMsg = res.data.message
+          }
+          this.fetching = false
+        })
+    },
+    copyThis(event) {
+      this.$nuxt.$emit('copyThis', event.target)
+    },
+  },
+}
+</script>
---
+++ components/QuickPasswords.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="p-4 border border-solid border-2 border-black">
+    <h3 class="text-2xl font-semibold tracking-wide">Quick Passwords</h3>
+    <table class="mt-4">
+      <thead class="border-solid border-b-2 border-gray-300">
+        <tr>
+          <th class="px-4 py-2 border-r-2 border-gray-300">Password</th>
+          <th class="px-4 py-2">Hashed Password</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="border-solid border-b border-gray-200">
+          <td class="px-4 py-2 border-r-2 border-gray-300">password</td>
+          <td class="px-4 py-2">
+            <span class="px-2 py-1" @click="copyThis"
+              >$2y$13$U/SK/GrCZH7qXHY8zztwTOE.Qrsj9BK8X72VwN7B6PK0fJAitQ5au</span
+            >
+          </td>
+        </tr>
+        <tr class="border-solid border-b border-gray-200">
+          <td class="px-4 py-2 border-r-2 border-gray-300">admin</td>
+          <td class="px-4 py-2">
+            <span class="px-2 py-1" @click="copyThis"
+              >$2y$13$QGycz2JvSXek1U4tkcQYr.NZR.hzGtWs1phfajD32u6BxaCjIkV5C</span
+            >
+          </td>
+        </tr>
+        <tr class="border-solid border-b border-gray-200">
+          <td class="px-4 py-2 border-r-2 border-gray-300">user</td>
+          <td class="px-4 py-2">
+            <span class="px-2 py-1" @click="copyThis"
+              >$2y$13$dKONpx.zD5A5lfKVcbIq6.2CTuHRgjPPibdEOMF8VK3J/StMApOEm</span
+            >
+          </td>
+        </tr>
+        <tr>
+          <td class="px-4 py-2 border-r-2 border-gray-300">craftcms</td>
+          <td class="px-4 py-2">
+            <span class="px-2 py-1" @click="copyThis"
+              >$2y$13$.2X4qOW5C099t7hBqiVydOJsO.JmyDeRLdgdEIxf3qnHK5dMywsiW</span
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      // any component data
+    }
+  },
+  methods: {
+    copyThis(event) {
+      this.$nuxt.$emit('copyThis', event.target)
+    },
+  },
+}
+</script>
