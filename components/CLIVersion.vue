a0f6e4c32dbd0fae55b5eb57726382c3c8e20078
James Nock <james.nock@dawsonandrews.com>
2021-07-11 18:48:51 +0100

    Add explanation under CLI version

--- components/CLIVersion.vue
+++ components/CLIVersion.vue
@@ -8,6 +8,8 @@
       curl -O https://craftcmspwgen.james-nock.co.uk/craftcmspwgen.php<br>
       echo 'alias craftpwgen="php ~/craftcmspwgen.php"' >> ~/.bash_profile
     </div>
+    <p class="py-2">Once you've reloaded your terminal you can then generate a hashed password by running:</p>
+    <p class="p-2 font-mono bg-yellow-300" @click="copyThis">craftpwgen PASSWORD</p>
   </div>
 </template>
 <script>
